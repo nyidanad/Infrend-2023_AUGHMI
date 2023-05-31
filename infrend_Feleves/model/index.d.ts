826a873602bc48711882f8fd367bfa287ff86433
NyDaniel <91250016+nyidanad@users.noreply.github.com>
2023-06-01 01:42:28 +0200

    Add files via upload

---
+++ infrend_Feleves/model/index.d.ts
@@ -0,0 +1,35 @@
+export interface CustomerDTO {
+    id: number;
+    fname: string;
+    lname: string;
+    address: string;
+    idCardNum: string;
+    phone: string;
+}
+
+export interface VehicleDTO {
+    id: number;
+    brand: string;
+    type: string;
+    regNum: string;
+    purchaseDate: Date;
+    rentalFee: number;
+    mileage: number;
+    status: string;
+    imgUrl: string;
+}
+
+export interface RentalDTO {
+    id: number;
+    rentalDate: Date;
+    customers: CustomerDTO;
+    vehicles: VehicleDTO;
+}
+
+export interface RetrieveDTO {
+    id: number;
+    damaged: boolean;
+    sum: number;
+    rentals: RentalDTO;
+    vehicles: VehicleDTO;
+}
